252b6ab47294dcb43cc2058fc40e3afc64424525
Kishore Balaji P <kishorebalaji880@gmail.com>
2025-10-13 15:19:05 +0530

    chore: app.test.js added jest,babel

--- app.test.js
+++ app.test.js
@@ -1,16 +1,14 @@
-// app.test.js
 import request from "supertest";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-// Recreate your Express app for testing
+// Create app for testing
 const app = express();
-app.use(cors({ origin: "*" }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Your existing route from index.js
 app.get("/", (req, res) => {
   res.json({ message: "Lambda Express is working!" });
 });
